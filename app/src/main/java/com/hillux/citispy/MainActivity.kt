3f2096a6e79140d48d39e43c6d8cc81c56a02a48
Hillary <hillary.kamau@eadatahandlers.co.ke>
2020-03-12 17:33:33 +0300

    added drawer menu

--- app/src/main/java/com/hillux/citispy/MainActivity.kt
+++ app/src/main/java/com/hillux/citispy/MainActivity.kt
@@ -1,21 +1,20 @@
 package com.hillux.citispy
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.coordinatorlayout.widget.CoordinatorLayout
-import com.google.android.material.appbar.AppBarLayout
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import androidx.appcompat.widget.Toolbar
-
-import android.widget.Toast
+import android.R
 import android.content.Intent
+import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ProgressBar
-import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.AdditionalUserInfo
+
 
 class MainActivity : AppCompatActivity() {
     private var btnTagUser: Button? = null
@@ -37,8 +36,30 @@ class MainActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         user = FirebaseAuth.getInstance().currentUser
 
+        setSupportActionBar(findViewById(R.id.app_bar))
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
 
+// There is usually only 1 header view.
+// Multiple header views can technically be added at runtime.
+// We can use navigationView.getHeaderCount() to determine the total number.
+        // There is usually only 1 header view.
+// Multiple header views can technically be added at runtime.
+// We can use navigationView.getHeaderCount() to determine the total number.
 
+        // Lookup navigation view
+        // Lookup navigation view
+        val navigationView =
+            findViewById<View>(R.id.navView) as NavigationView
+// Inflate the header view at runtime
+// Inflate the header view at runtime
+        val headerLayout = navigationView.inflateHeaderView(R.layout.nav_header)
+        if (navigtationView.getHeaderCount() > 0) {
+            // avoid NPE by first checking if there is at least one Header View available
+            val headerLayout: View = navigationView.getHeaderView(0)
+        }
     }
 
     override fun onStart() {
