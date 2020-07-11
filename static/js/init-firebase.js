077b5373cdec61e620a5e1e40be81b54e4edc639
Hillary-KG <gachukihillary@gmail.com>
2020-07-11 03:43:59 +0300

    this commit adds firebase datatabse reader and fcm

---
+++ static/js/init-firebase.js
@@ -0,0 +1,14 @@
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA-atkaTHhE-fJm7hcr4njC4_elRa3DrnQ",
+    authDomain: "citispy-91191.firebaseapp.com",
+    databaseURL: "https://citispy-91191.firebaseio.com",
+    projectId: "citispy-91191",
+    storageBucket: "citispy-91191.appspot.com",
+    messagingSenderId: "629555630146",
+    appId: "1:629555630146:web:53153b172b74c280559ddb",
+    measurementId: "G-TNXTNTCWG5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
